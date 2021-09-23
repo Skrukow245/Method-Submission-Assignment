42da1c82f9e6e2d9e6fc5ca046ddde3f017ea40b
Skrukow245 <skrukow245@gmail.com>
2021-09-23 12:37:23 -0600

    Add project files.

---
+++ Method Submission Assignment/Operator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+public class Operator
+{
+    public int Add(int input1, int input2 = 5)
+    {
+        return input1 + input2;
+    }
+}
+
---
+++ Method Submission Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var addition = new Operator();                    //instantiate method
+        Console.WriteLine("Input 1 number");
+        int input1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter another number (optional)");
+        int input2;
+        int result;
+        if (int.TryParse(Console.ReadLine(), out input2))              //if console read is empty assign it to input2
+        {
+            result = addition.Add(input1, input2);
+        }
+        else                                                           //else if user input add with input 1
+        {
+            result = addition.Add(input1);
+        }
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
+
